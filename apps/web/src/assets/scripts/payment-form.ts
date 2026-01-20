94d7c55d6a102132f4be8c03d9135d6ea32d92de
Matthew Booth <matthew@boov.co.uk>
2026-01-20 13:56:19 +0000

    FIXED: Bug with payment form amount

--- apps/web/src/assets/scripts/payment-form.ts
+++ apps/web/src/assets/scripts/payment-form.ts
@@ -3,16 +3,16 @@ export default () => ({
   // email: "",
 
   formatAmount() {
-    const amountInCents = parseInt(this.amount.replace(/\D/g, "")) * 100;
+    const amountInPence = this.amount * 100;
 
-    return amountInCents ? amountInCents : 0;
+    return amountInPence ? amountInPence : 0;
   }
 
   // formURL() {
   //   const link_id = "test_7sY4gBddRbov0mMf229EI00";
-  //   const amountInCents = parseInt(this.amount.replace(/\D/g, "")) * 100;
+  //   const amountInPence = parseInt(this.amount.replace(/\D/g, "")) * 100;
   //   const email = encodeURIComponent(this.email);
   //
-  //   return `https://buy.stripe.com/${link_id}?__prefilled_amount=${amountInCents ? amountInCents : 0}&prefilled_email=${email}`;
+  //   return `https://buy.stripe.com/${link_id}?__prefilled_amount=${amountInPence ? amountInPence : 0}&prefilled_email=${email}`;
   // }
 });
